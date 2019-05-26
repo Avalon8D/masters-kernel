a6d6f607f09bb58e3ddf6296d883a01591829417
Avalon8D <pedroaschmidt@gmail.com>
2019-05-26 12:24:12 -0300

    tweaked initialization action to account for new python 3 datarpoc image and changed spark dependencies accordingly

--- dataproc_scripts/python3_dataproc.sh
+++ dataproc_scripts/python3_dataproc.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 # dataproc small example
-# gcloud beta dataproc clusters create thesis-tiny-python3-anaconda --subnet default --zone us-central1-a --master-machine-type n1-highmem-2 --master-boot-disk-size 40 --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 40 --image-version 1.4-debian9 --project seo-analytics-219818 --optional-components=ANACONDA --initialization-actions 'gs://<bucket>/<path-to-initialization-action>/python3_dataproc.sh' --initialization-action-timeout 20m
+# gsutil cp ./python3_dataproc.sh gs://<bucket>/<path-to-initialization-action>/python3_dataproc.sh
+# gcloud beta dataproc clusters create thesis-tiny-python3-anaconda --subnet default --zone us-central1-a --master-machine-type n1-highmem-2 --master-boot-disk-size 40 --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 40 --image-version 1.4-debian9 --project seo-analytics-219818 --initialization-actions 'gs://<bucket>/<path-to-initialization-action>/python3_dataproc.sh' --initialization-action-timeout 20m
 
 # everything between <> should be filled with appropriate values
 
 echo "export PYTHONHASHSEED=0
-export AWS_SECRET_ACCESS_KEY=<aws_access_key>
-export AWS_ACCESS_KEY_ID=<aws_key_id>
-export PYSPARK_DRIVER_PYTHON=/opt/python3/bin/python
+export PYSPARK_DRIVER_PYTHON=python
 " | \
 tee -a /etc/profile.d/spark_config.sh /etc/*bashrc /usr/lib/spark/conf/spark-env.sh
 
 echo "spark.executorEnv.PYTHONHASHSEED=0
 spark.executorEnv.AWS_SECRET_ACCESS_KEY=<aws_access_key>
 spark.executorEnv.AWS_ACCESS_KEY_ID=<aws_key_id>
-export PYSPARK_DRIVER_PYTHON=/opt/python3/bin/python
 " >> /etc/spark/conf/spark-defaults.conf
 
 echo "$(head -n -1 /usr/lib/spark/bin/spark-submit)""export AWS_SECRET_ACCESS_KEY=<aws_access_key>
 export AWS_ACCESS_KEY_ID=<aws_key_id>
-export PYSPARK_DRIVER_PYTHON=/opt/python3/bin/python
+export PYSPARK_DRIVER_PYTHON=python
 ""$(tail -n 1 /usr/lib/spark/bin/spark-submit)" >> /usr/lib/spark/bin/spark-submit
 
 head /usr/lib/spark/bin/pyspark -n -3 > /usr/bin/jupyspark
-echo 'export PYSPARK_DRIVER_PYTHON=/opt/python3/bin/jupyter
+echo 'export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS=notebook
 exec ${SPARK_HOME}/bin/spark-submit pyspark-shell-main --name "PySparkShell" --packages com.amazonaws:aws-java-sdk-pom:1.11.466 --jars=gs://hadoop-lib/bigquery/bigquery-connector-hadoop2-latest.jar "$@"' >> /usr/bin/jupyspark
 
@@ -40,15 +38,14 @@ sudo apt update
 sudo apt install sbt -y
 
 # Install requirements
-conda create -p /opt/python3 python=3.7 -y
-conda install -p /opt/python3 pandas jupyter scipy -y
-conda install -p /opt/python3 numba -y -c numba
-# conda install -p /opt/python3 -c plotly plotly -y
-conda install -p /opt/python3 -c conda-forge jupyter_contrib_nbextensions jupyter_nbextensions_configurator pyarrow python-snappy -y
-conda install -p /opt/python3 -c r r
+conda install pandas jupyter scipy -y
+conda install numba -y -c numba
+# conda install -c plotly plotly -y
+conda install -c conda-forge jupyter_contrib_nbextensions jupyter_nbextensions_configurator pyarrow python-snappy -y
+conda install -c r r
 
 echo '#!/bin/bash
-/opt/python3/bin/jupyter notebook --allow-root --generate-config -y --ip=127.0.0.1
+jupyter notebook --allow-root --generate-config -y --ip=127.0.0.1
 echo "c.Application.log_level = '"'DEBUG'"'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.ip = '"'0.0.0.0'"'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
@@ -57,11 +54,12 @@ echo "c.NotebookApp.password = u'"'<notebook password hash>'"'" >> ~/.jupyter/ju
 
 chmod +x /usr/bin/config_jupyter
 
-/opt/python3/bin/pip install toree
-/opt/python3/bin/pip install --upgrade pip
-/opt/python3/bin/jupyter toree install --spark_home=/usr/lib/spark --interpreters=Scala,PySpark,SparkR,SQL
+pip install toree
+pip install --upgrade pip
+jupyter toree install --spark_home=/usr/lib/spark --interpreters=Scala,PySpark,SparkR,SQL
 
-conda update -p /opt/python3 --all -y
+conda update --all -y
 sudo apt upgrade -y
 
-tmux new-session -d -s "jupyter_session" "config_jupyter && /opt/python3/bin/jupyter notebook"
+echo 'tmux new-session -d -s "jupyter_session" "/usr/bin/config_jupyter && jupyter notebook"' >> /usr/bin/lift_jupyter
+chmod +x /usr/bin/lift_jupyter
--- scala_srcs/build.sbt
+++ scala_srcs/build.sbt
@@ -3,10 +3,10 @@ name := "kernel-lib"
 scalaVersion := "2.11.12"
 
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.2"
 
 // https://mvnrepository.com/artifact/org.apache.spark/spark-core
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.2"
 
 // https://mvnrepository.com/artifact/org.scalanlp/breeze-natives
 libraryDependencies += "org.scalanlp" %% "breeze-natives" % "1.0-RC2"
